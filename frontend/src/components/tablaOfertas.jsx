060388b5bf62f823e49e466e1982b4d6ab22dcec
PieritoAlva95 <jpalvaradoc@unl.edu.ec>
2021-07-14 10:27:32 -0500

    Cambios en la UI

--- frontend/src/components/tablaOfertas.jsx
+++ frontend/src/components/tablaOfertas.jsx
@@ -1,42 +1,59 @@
 import React, { Fragment } from 'react';
-import EditarOferta from './editOferta'
+import EditarOferta from './editOferta';
 import { Link } from 'react-router-dom';
 
-
 const TablaOfertas = ({ oferta, metodoCargarDatos }) => {
-    const user = JSON.parse(window.localStorage.getItem('user'));
+  const user = JSON.parse(window.localStorage.getItem('user'));
 
-
-    const eliminarOferta = async () => {
-        const requestOptions = {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-token': user.token
-            }
-        };
-        const response = await fetch('http://localhost:4000/api/oferta/'+oferta._id, requestOptions);
-        const data = await response.json();
-        console.log(data);
-        metodoCargarDatos()
-        // alert(oferta._id)
-    }
-
-    return (
-        <Fragment>
-            <tr>
-                <td>{oferta.titulo}</td>
-                <td>{oferta.cuerpo}</td>
-                <td>
-                    <Link className="btnLink" to={{pathname:"/dashboard/visualizar-oferta", state:{oft:oferta} }}><i className='bx bxs-show'></i></Link>
-                    <Link className="btnLink" to={{pathname:"/dashboard/editar-oferta", state:{oft:oferta} }}><i className='bx bx-edit'></i></Link>
-                    <button type="button" className="btnDelete" onClick={eliminarOferta}>
-                        <i className='bx bx-trash'></i>
-                    </button>
-                </td>
-            </tr>
-        </Fragment>
+  const eliminarOferta = async () => {
+    const requestOptions = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-token': user.token,
+      },
+    };
+    const response = await fetch(
+      'http://localhost:4000/api/oferta/' + oferta._id,
+      requestOptions
     );
-}
+    const data = await response.json();
+    console.log(data);
+    metodoCargarDatos();
+    // alert(oferta._id)
+  };
 
-export default TablaOfertas
+  return (
+    <Fragment>
+      <tr>
+        <td>{oferta.titulo}</td>
+        <td>{oferta.cuerpo}</td>
+        <td>
+          <Link
+            className='btnLink'
+            to={{
+              pathname: '/dashboard/visualizar-oferta',
+              state: { oft: oferta },
+            }}
+          >
+            <i className='bx bxs-show'></i>
+          </Link>
+          <Link
+            className='btnLink'
+            to={{
+              pathname: '/dashboard/editar-oferta',
+              state: { oft: oferta },
+            }}
+          >
+            <i className='bx bx-edit'></i>
+          </Link>
+          <button type='button' className='btnDelete' onClick={eliminarOferta}>
+            <i className='bx bx-trash'></i>
+          </button>
+        </td>
+      </tr>
+    </Fragment>
+  );
+};
+
+export default TablaOfertas;
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -3,75 +3,102 @@ import { useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import image from '../assets/worker.jpg';
 
-const LoginPage = ({setLogeado}) => {
-    const history = useHistory();
-    const [usuario, setusuario] = useState({})
+const LoginPage = ({ setLogeado }) => {
+  const history = useHistory();
+  const [usuario, setusuario] = useState({});
 
-    const handleInputChange = e => {
-        const { name, value } = e.target
-        setusuario({ ...usuario, [name]: value })
-    }
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setusuario({ ...usuario, [name]: value });
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        login(usuario)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(usuario);
+  };
 
-    const login = async (user) => {
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(user)
-        };
-        const response = await fetch('http://localhost:4000/api/login', requestOptions);
-        const data = await response.json();
-        if (data.ok == true) {
-            window.localStorage.setItem('user', JSON.stringify(data));
-            setLogeado(true)
-            history.push("/dashboard");
-        } else {
-            alert("Usuario no encontrado");
-        }
-    }
-
-    return (
-        <div className="container mainlogin">
-            <div className="row">
-                <div className="col-lg-6 imagen">
-                    <img src={image} alt="" />
-                </div>
-                <div className="col-lg-6 formulario">
-                    <form className="login-form needs-validation" onSubmit={handleSubmit}>
-                        <h3>Login</h3>
-
-                        <div className="form-group">
-                            <label>Email address</label>
-                            <input type="email" className="form-control" placeholder="Ingresa un correo" name="email" onChange={handleInputChange} required />
-                        </div>
-
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input type="password" className="form-control" placeholder="Ingresa tu contraseña" name="password" onChange={handleInputChange} required />
-                        </div>
-                        <div id="msj-error" className="mensaje-error disable">
-                            <p>LO SIENTO EL USUARIO NO HA SIDO ENCONTRADO EN NUESTROS REGISTROS</p>
-                        </div>
-                        <div className="form-group">
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                                <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                            </div>
-                        </div>
-
-                        <button type="submit">Ingresar</button>
-                        <p className="forgot-password text-right">
-                            Forgot password?
-                        </p>
-                        <Link className="link" to="/registro">Registrate!</Link>
-                    </form>
-                </div>
-            </div>
-        </div>
+  const login = async (user) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user),
+    };
+    const response = await fetch(
+      'http://localhost:4000/api/login',
+      requestOptions
     );
-}
+    const data = await response.json();
+    if (data.ok == true) {
+      window.localStorage.setItem('user', JSON.stringify(data));
+      setLogeado(true);
+      history.push('/dashboard');
+    } else {
+      alert('Usuario no encontrado');
+    }
+  };
+
+  return (
+    <div className='container mainlogin'>
+      <div className='row'>
+        <div className='col-lg-6 imagen'>
+          <img src={image} alt='' />
+        </div>
+        <div className='col-lg-6 formulario'>
+          <form className='login-form needs-validation' onSubmit={handleSubmit}>
+            <h3>Login</h3>
+
+            <div className='form-group'>
+              <label>Email address</label>
+              <input
+                type='email'
+                className='form-control'
+                placeholder='Ingresa un correo'
+                name='email'
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+
+            <div className='form-group'>
+              <label>Contraseña</label>
+              <input
+                type='password'
+                className='form-control'
+                placeholder='Ingresa tu contraseña'
+                name='password'
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div id='msj-error' className='mensaje-error disable'>
+              <p>
+                LO SIENTO EL USUARIO NO HA SIDO ENCONTRADO EN NUESTROS REGISTROS
+              </p>
+            </div>
+            <div className='form-group'>
+              <div className='custom-control custom-checkbox'>
+                <input
+                  type='checkbox'
+                  className='custom-control-input'
+                  id='customCheck1'
+                />
+                <label className='custom-control-label' htmlFor='customCheck1'>
+                  Recuerdame
+                </label>
+              </div>
+            </div>
+
+            <button type='submit'>Ingresar</button>
+            <p className='forgot-password text-right'>
+              Has olvidado tu contraseña?
+            </p>
+            <Link className='link' to='/registro'>
+              Regístrate!
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default LoginPage;
